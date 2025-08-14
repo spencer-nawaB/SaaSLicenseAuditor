25f273bf7f75c9a08f3cbcdcc769c82c042ffb26
spencer-nawaB <spencer@marketspire.ai>
2025-08-14 16:55:56 -0700

    Add files via upload

---
+++ app:page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+// Copy the entire SaaSLicenseAuditor component code here
+import React, { useState, useEffect, useMemo } from 'react';
+// ... rest of your component code
+
+export default function Home() {
+  return <SaaSLicenseAuditor />;
+}
