1ab99837416adf56686a61131286c67a98cffe1f
spencer-nawaB <spencer@marketspire.ai>
2025-08-14 17:20:37 -0700

    Delete app:page.tsx

--- app:page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-// Copy the entire SaaSLicenseAuditor component code here
-import React, { useState, useEffect, useMemo } from 'react';
-// ... rest of your component code
-
-export default function Home() {
-  return <SaaSLicenseAuditor />;
-}
